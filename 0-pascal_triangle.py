209032b46163276bfde093bb87b8291eff89e2d2
Andre victoria <122878923+Saint8083@users.noreply.github.com>
2024-01-08 22:16:43 +0100

    pascal triangle

--- 0-pascal_triangle.py
+++ 0-pascal_triangle.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-"""Pascal Triangle"""
-
+"""Writing a function for Pascal's Triangle"""
 
 def pascal_triangle(n):
-    """returns a list of lists of numbers
+    """returns a list of lists of integers
     representing the pascal triangle of (n)"""
     if n <= 0:
         return []
